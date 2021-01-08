f9c37c7a092ac24034a0cdb71bd69cecf819dadb
Itumeleng <thiotsoela@gmail.com>
2021-01-08 16:51:41 +0200

    profile section

--- src/components/landingpage/Navbard.vue
+++ src/components/landingpage/Navbard.vue
@@ -8,7 +8,7 @@
                     <li v-if="$store.state.token" @click="logoutTheUser" class="signup special"><a to="javascript:void(0)">Log Out</a></li>
                     <li @click="navigate('Favourite')"><i class="fas fa-heart justico"></i></li>
                     <li v-if="$store.state.isAdmin" @click="navigate('Dashboard')" ><i class="icofont-dashboard-web pri-colour"></i></li>
-                    <li><i class="icofont-user-alt-7 justico"></i></li>
+                    <li v-if="$store.state.token"><i class="icofont-user-alt-7 justico"></i></li>
                     <li class="cart"><i @click="navigate('Cart')" class="icofont-shopping-cart ico"></i><h1 class="items-the-cart">{{getShoppingcart}}</h1></li>
                 </ul>
             </section>
